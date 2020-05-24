6e2fd3bd1edfb806c589a824722e2e09483c90b6
daddyjoys <ingener1c@list.ru>
2020-05-24 14:05:32 +0500

    patterns factory + test

--- src/main/java/com/daddyjoys/java/patterns/factory/Cherry.java
+++ src/main/java/com/daddyjoys/java/patterns/factory/Cherry.java
@@ -1,4 +1,9 @@
 package com.daddyjoys.java.patterns.factory;
 
-public class Cherry {
+public class Cherry implements Factory {
+    @Override
+    public void eat() {
+        System.out.println("Chocolate");
+    }
+
 }
--- src/main/java/com/daddyjoys/java/patterns/factory/Chocolate.java
+++ src/main/java/com/daddyjoys/java/patterns/factory/Chocolate.java
@@ -1,4 +1,8 @@
 package com.daddyjoys.java.patterns.factory;
 
-public class Chocolate {
+public class Chocolate implements  Factory {
+    @Override
+    public void eat() {
+        System.out.println("Chocolate");
+    }
 }

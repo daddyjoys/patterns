a4c84370e4e76a906556d7eb2a09acc42e0aaa4f
daddyjoys <ingener1c@list.ru>
2020-06-14 18:54:54 +0500

    Collections Set

---
+++ src/main/java/com/daddyjoys/java/collections/MyHashSet.java
@@ -0,0 +1,12 @@
+package com.daddyjoys.java.collections;
+
+import java.util.HashSet;
+
+public class MyHashSet {
+    public static void main(String[] args){
+        HashSet<String> myHashSet = new HashSet<String>();
+        myHashSet.add(1);
+        myHashSet.add(2);
+        myHashSet.add(3);
+    }
+}
